a8c1140d4df7ef6ade6f482969d4cac704654cfa
Mauricio Donado <donadom@utb.edu.co>
2024-09-07 00:26:24 -0500

    modificacion

---
+++ wpsUI/src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { HomePagee } from "@/components/component/layoutpage";
+import { Layout } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return <HomePagee />;
+}
